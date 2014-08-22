6a8e8fe48c2b5ef927ab268c548f8d31491def22
Marc-Antoine Ruel <maruel@chromium.org>
2014-08-21 20:50:20 -0400

    Fix presubmit check again.
    I really need to make this a git commit hook, soon.

--- wi-plugin/plugin_main_test.go
+++ wi-plugin/plugin_main_test.go
@@ -12,7 +12,7 @@ func TestCalculateVersion(t *testing.T) {
 	v := CalculateVersion()
 	// TODO(maruel): We don't care about the actual version. Just test the
 	// underlying code to calculate a version is working.
-	if v != "c3a9c8da157bdff510e5d117cf299d094271d9b5" {
+	if v != "308efa67a5805b8f3623c3a75a4f0cc7ac1ca1a8" {
 		t.Fatal(v)
 	}
 }
