77aa4a7f65f2eb710518677a0cc368dccaa104f5
Marc-Antoine Ruel <maruel@chromium.org>
2014-09-22 10:09:46 -0400

    Doc finetuning

--- wi-plugin/doc.go
+++ wi-plugin/doc.go
@@ -2,8 +2,8 @@
 // Use of this source code is governed under the Apache License, Version 2.0
 // that can be found in the LICENSE file.
 
-// Package wi (wi-plugin) implements all the interfaces to be shared between
-// the wi editor process and its plugins.
+// Package wi implements all the interfaces to be shared between the wi editor
+// process and its plugins.
 //
 // It is strongly versioned via the SHA-1 of its interface.
 package wi
