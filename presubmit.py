0da47f5322c3c9b0f93bebfbf2ea460ca499f67a
Marc-Antoine Ruel <maruel@chromium.org>
2015-01-06 08:36:29 -0500

    Add presubmit to build go build -tags debug too

--- presubmit.py
+++ presubmit.py
@@ -1,1 +1,20 @@
-git-hooks-go/presubmit.py
+#!/usr/bin/env python
+# Copyright 2014 Marc-Antoine Ruel. All rights reserved.
+# Use of this source code is governed under the Apache License, Version 2.0
+# that can be found in the LICENSE file.
+
+"""Copy this script in your package if desired."""
+
+import os
+import sys
+
+
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.join(THIS_DIR, 'git-hooks-go'))
+
+import presubmit_impl
+
+
+if __name__ == '__main__':
+  # Disable golint or govet if desired.
+  sys.exit(presubmit_impl.main(tags='debug', run_golint=True, run_govet=True))
